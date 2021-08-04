8066fc70559e69870ceda84d74a6161413d56bdd
Woska <60791044+agwoska@users.noreply.github.com>
2021-08-04 13:39:26 -0400

    Add files via upload

---
+++ src/app/base.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import ReactCSSTransitionGroup from 'react-transition-group';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  
+}
+
+export default App;
