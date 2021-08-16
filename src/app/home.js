2a1d3bc02c36e77101bb93776c6b151440fbfe56
agwoska <agwoska@buffalo.edu>
2021-08-15 22:18:56 -0400

    Create basic homepage

---
+++ src/app/home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import '../animations/fadeUp.css'
+
+const Greetings = () => {
+  const opening = 'Welcome to Woska.net';
+  const [letters, setLetter] = useState('');
+  const [l, setL] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setL(l+1);
+      setLetter(opening.substring(0, l));
+    }, 100);
+    return () => clearInterval(timer)
+  });
+  return (
+    <div id="Open">
+      <h1>{letters}</h1>
+    </div>
+  )
+}
+
+function Intro() {
+  return (
+    <div id='intro' className='fadeUp'>
+      <h4>
+        Greetings! You have arrived at my website.<br /><br />
+        Here you will find some of my apps, notes,
+        and other creations I have made over the years.<br />
+        If you are interested in learning about me and what I
+        do, feel free to click on the About Me tab at the top.<br />
+        If you wish to contact me, click on Contact Me tab and fill out the
+        form or just send me an email.
+      </h4>
+    </div>
+  )
+}
+
+function Home() {
+
+  return (
+    <>
+      <Greetings /><br />
+      <Intro />
+    </>
+  )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/base';
+import App from './app/home';
 
 ReactDOM.render(
   <App />,
