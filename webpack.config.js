8545b7b30e49f50e8747895af402819142b201e0
Woska <andrew@woska.org>
2024-01-06 20:26:33 -0500

    fix bundle

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        // path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 3000,
+        historyApiFallback: true,
+        allowedHosts: [
+            'localhost',
+            'woska.net',
+            'fonts.googleapis.com',
+        ],
+        // hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: ['url-loader'],
+                type: 'asset/resource'
+            }
+        ],
+    },
+};
